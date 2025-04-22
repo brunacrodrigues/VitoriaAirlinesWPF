6e44e599058de8866c879e9b526ed89a9e276e17
brcrodrigues96@gmail.com <brcrodrigues96@gmail.com>
2025-04-22 13:44:25 +0100

    Add project files.

---
+++ VitoriaAirlinesWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VitoriaAirlinesWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
